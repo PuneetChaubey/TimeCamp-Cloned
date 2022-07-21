df6356a6919dc436e71151f304761113c80a053c
PuneetChaubey <puneeteklavya@gmail.com>
2022-07-21 20:13:19 +0530

    done some body part of integrations page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import { MainRoutes } from "./Pages/MainRoutes";
 import LandingPAge from "./Pages/LandingPAge";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
 function App() {
   return (
     <div className="App">
+  
       <MainRoutes />
-      <LandingPAge />
+   
     </div>
   );
 }
