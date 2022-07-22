f7249a9afd5aa070f46e3e6c8452ac0930d863c6
Sridhar-10 <sridharchandhiran@gmail.com>
2022-07-22 20:07:36 +0530

    project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import { MainRoutes } from "./Pages/MainRoutes";
 import Stopwatch from "./Components/Stopwatch";
+import Project from "./Pages/Project";
 function App() {
   return (
     <div  >
       <Stopwatch/>
       <MainRoutes />
+      <Project/>
    
     </div>
   );
